f7fdddad1119bb332451892015bd467c21db32c2
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-07-19 17:29:36 +0530

    (fix) backup structure fix

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const wa = require("@open-wa/wa-automate");
 const mime = require('mime-types');
 const fs = require('fs');
 
+const testNumber = "";
 const hostname = '127.0.0.1';
 const port = 8081;
 
 wa.create().then(async (client) => {
-  backUpContacts(client);
-  backUpChats(client);
-  backUpMedia(client);
+  await Promise.all([
+    backUpContacts(client),
+    backUpChats(client),
+    backUpMedia(client),
+    // backUpChatMsgs(client,`${testNumber}@c.us`)
+  ]).then(() => {
+    console.log("completed");
+  })
 });
 
 const writeFile = (name, data) => {
@@ -28,38 +34,57 @@ const server = http.createServer((req, res) => {
 
 const backUpContacts = async (client) => {
   const contacts = await client.getAllContacts().then((cnts) => { return JSON.stringify(cnts) });
-  writeFile("./backup/contacts.json", contacts);
+  writeFile("./backup/contacts.txt", contacts);
 }
 
 const backUpChats = async (client) => {
+  let runCount = 0;
   await client.getAllChatIds().then((ids) => {
     ids.map(async (id) => {
-      await client.getAllMessagesInChat(id, true).then((message) => {
-        const filename = `./backup/chat/${id}.json`;
+      await client.loadAndGetAllMessagesInChat(id, true).then((message) => {
+        const filename = `./backup/chat/${id}.txt`;
         writeFile(filename, JSON.stringify(message));
-      })
-    })
+        runCount++;
+        console.log(`${runCount} chat backedup`);
+      }).catch(() => {
+        runCount++;
+        console.log(`${runCount} error on backup with id ${id}`);
+      });
+    });
+  console.log(`${ids.length} chat retrevied please wait while backup is in process`);
   });
 }
 
 const backUpMedia = async (client) => {
   await client.getAllChatIds().then((ids) => {
     ids.map(async (id) => {
-      await client.getAllMessagesInChat(id, true).then((messages) => {
-        messages.map(async(message)=>{
+      await client.loadAndGetAllMessagesInChat(id, true).then((messages) => {
+        messages.map(async (message) => {
           if (message.mimetype) {
-            const filename = `${message.t}.${mime.extension(message.mimetype)}`;
-            let mediaData = await wa.decryptMedia(message).catch((err)=>{
-              if(err.response.status == 404) {
-                mediaData = wa.decryptMedia(client.forceStaleMediaUpdate(message.id));
-                writeFile(filename, mediaData);
-              }
-            })
-          writeFile(filename, mediaData);
+            const filename = `./backup/media/${message.sender.id}/${message.t}.${mime.extension(message.mimetype)}`;
+            let mediaData = await wa.decryptMedia(message).then(() => {
+              fs.mkdirSync(`./backup/media/${message.sender.id}`, { recursive: true })
+              writeFile(filename, mediaData);
+            }).catch(() => {
+              console.log(`cannot get ${message.t}.${mime.extension(message.mimetype)}`);
+            });
           }
-        })
-      })
-    })
+        });
+      });
+    });
+  });
+}
+
+const backUpChatMsgs = async (client, id) => {
+  client.loadAndGetAllMessagesInChat(id, true).then((messages) => {
+    messages.map(async (message) => {
+      if (message.mimetype) {
+        const filename = `./backup/media/${message.sender.id}/${message.t}.${mime.extension(message.mimetype)}`;
+        let mediaData = await wa.decryptMedia(message)
+        fs.mkdirSync(`./backup/media/${message.sender.id}`, { recursive: true })
+        writeFile(filename, mediaData);
+      }
+    });
   });
 }
 
